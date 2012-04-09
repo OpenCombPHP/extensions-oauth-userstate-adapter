5a3821efefda15fd08b9b8064e74b06e87ac0af1
kongyuan <anubiskong@gmail.com>
2012-04-09 18:00:01 +0800

    ...

--- class/OAuth_userstate_adapter.php
+++ class/OAuth_userstate_adapter.php
@@ -2,11 +2,8 @@
 namespace org\opencomb\oauth_userstate_adapter ;
 
 use org\jecat\framework\ui\xhtml\weave\Patch;
-
 use org\jecat\framework\lang\aop\AOP;
-
 use org\jecat\framework\ui\xhtml\weave\WeaveManager;
-
 use org\opencomb\platform\ext\Extension ;
 
 class OAuth_userstate_adapter extends Extension 
@@ -35,56 +32,3 @@ class OAuth_userstate_adapter extends Extension
 		$aWeaveMgr->registerTemplate( 'userstate:UserState.html', "/div@2/model:foreach@0/dl@0/dd@0/div@2/div@0/textarea@0", 'oauth_userstate_adapter:aspect/ForwardState.html', Patch::appendAfter ) ;
 	}
 }
-
-
-/*
-namespace org\opencomb\oauth_userstate_adapter;
-
-use org\jecat\framework\ui\xhtml\weave\Patch;
-
-use org\jecat\framework\lang\aop\AOP;
-
-use org\jecat\framework\ui\xhtml\weave\WeaveManager;
-
-use org\opencomb\platform\ext\Extension;
-
-class OAuth_userstate_adapter extends Extension {
-	public function load() {
-
-		// 获取最新记录数然时候先拉取
-		AOP::singleton ()->register ( 'org\\opencomb\\oauth_userstate_adapter\\aspect\\UserStatePullStateAspect' );
-
-		// 点击更多时拉取
-		// AOP::singleton()->register('org\\opencomb\\oauth_userstate_adapter\\aspect\\ToListStateAspect')
-		// ;
-
-		// 发布消息同步到weibo
-		AOP::singleton ()->registerBean ( array (
-		// jointpoint
-		'org\\opencomb\\friends\\RemoveFriend::process()',
-		// advice
-		array (
-		'org\\opencomb\\oauth_friends_adapter\\aspect\\RemoveFriendAspect',
-		'process'
-		)
-		), __FILE__ )->registerBean ( array (
-		// jointpoint
-		'org\\opencomb\\friends\\CreateFriend::process()',
-		// advice
-		array (
-		'org\\opencomb\\oauth_userstate_adapter\\aspect\\UserStatePushStateAspect',
-		'process'
-		)
-		), __FILE__ );
-
-		$aWeaveMgr = WeaveManager::singleton ();
-
-		$aWeaveMgr->registerTemplate ( 'userstate:CreateState.html', "/form@0/div@0/div@0/div@0/div@1", 'oauth_userstate_adapter:aspect/pushState.html', Patch::insertBefore );
-
-		// 拉取
-		$aWeaveMgr->registerTemplate ( 'userstate:UserState.html', "/div@2/model:foreach@0/script@0", 'oauth_userstate_adapter:aspect/ToListState.html', Patch::insertAfter );
-
-		// 转发
-		$aWeaveMgr->registerTemplate ( 'userstate:UserState.html', "/div@2/model:foreach@0/dl@0/dd@0/div@2/div@0/textarea@0", 'oauth_userstate_adapter:aspect/ForwardState.html', Patch::appendAfter ) ;
-	}
-}*/
